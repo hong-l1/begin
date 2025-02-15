bb3dd917675e8efc4ec29b87b6509834218a4dcc
jiahao <lijiahao@123.com>
2025-02-15 13:19:05 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "project/test"
+
+func main() {
+	test.Add(1, 2)
+	test.Chu(1, 2)
+	test.Plus(1, 2)
+	test.Sub(1, 2)
+}
---
+++ test/main.go
@@ -0,0 +1,16 @@
+package test
+
+import "fmt"
+
+func Add(x, y int) {
+	fmt.Println(x + y)
+}
+func Sub(x, y int) {
+	fmt.Println(x - y)
+}
+func Plus(x, y int) {
+	fmt.Println(x * y)
+}
+func Chu(x, y int) {
+	fmt.Println(x / y)
+}
